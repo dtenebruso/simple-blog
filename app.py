c515fd6475eea2d8b2f50112fefdb1790a08b8ad
dtenebruso <dtenebruso92@gmail.com>
2017-12-12 19:03:59 -0500

    create database class/add more to post.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 __author__ = 'pr0c'
 
+from database import Database
 from models.post import Post
+
+Database.initialize()
 
 post = Post("Post1 title", "Post1 content","Post1 author")
 post2 = Post("Post2 title", "Post2 content","Post2 author")
